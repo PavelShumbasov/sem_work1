9e9a0d73e0395719fbef8f3a77af1837c934199f
Pavel <pavelshumbasov2335@gmail.com>
2021-11-04 21:35:19 +0300

    Добавил меню, html-шаблоны

---
+++ web_site/menu.py
@@ -0,0 +1,6 @@
+menu = [{'name': 'Главная', 'url': 'views.home'},
+        {'name': 'Мои доски', 'url': 'views.my_boards'},
+        {'name': 'Создать доску', 'url': 'views.add_board'},
+        {'name': 'Редактировать аккаунт', 'url': 'auth.edit_profile'},
+        {'name': 'Выйти из аккаунта', 'url': 'auth.logout'},
+        {'name': 'Регистрация', 'url': 'auth.sign_up'}]
